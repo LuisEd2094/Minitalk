68191efd9fe45c53e61f4c0f6d174b7c3eebfcf7
Luis <luissoto2094@gmail.com>
2023-06-08 13:44:51 +0200

    removed exe.d.o and created gitignore

--- deps/client/client.d
+++
@@ -1,3 +0,0 @@
-obj/client/client.o: client_src/client.c Libft/includes/ft_printf.h
-
-Libft/includes/ft_printf.h:
--- deps/server/server.d
+++
@@ -1,3 +0,0 @@
-obj/server/server.o: server_src/server.c Libft/includes/ft_printf.h
-
-Libft/includes/ft_printf.h:
